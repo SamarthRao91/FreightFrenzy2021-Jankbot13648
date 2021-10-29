7f884c72dcb210ef6a152bd786581abacf44bea1
Samarth Rao <raosamarth91@gmail.com>
2021-10-29 01:17:37 -0700

    turret subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Turret.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Turret.java
@@ -12,14 +12,42 @@ import org.firstinspires.ftc.teamcode.Constants;
 
 public class Turret {
     public Servo turret;
+
     public Turret (HardwareMap hardwareMap)
     {
-        turret = hardwareMap.get(Servo.class, Constants.EXTENDER_SERVO_NAME);
+        turret = hardwareMap.get(Servo.class, Constants.TURRET_SERVO_NAME);
 
     }
     public void turretPresets(double position)
     {
+        if(Constants.elevator_postion < Constants.elevator_minimumsafepos && (position == Constants.turret_zeropos)
+         || (Constants.ex_position > 0 || Constants.elevator_safepos < Constants.elevator_minimumsafepos) &&
+                Constants.elevator_postion < Constants.elevator_safepos && ((position > Constants.turret_right_safepos &&
+                Constants.turret_position < Constants.turret_right_safepos) || (position < Constants.turret_left_safepos &&
+                Constants.turret_position > Constants.turret_right_safepos )))
+        {
+            return;
+        }
+        if(position > Constants.turret_right_safepos)
+        {
+            turret.setPosition(Constants.turret_right_safepos);
+            Constants.turret_position = Constants.turret_right_safepos;
+        }
+        else if(position< Constants.turret_left_safepos)
+        {
+            turret.setPosition(Constants.turret_left_safepos);
+            Constants.turret_position = Constants.turret_left_safepos;
+        }
         turret.setPosition(position);
+        Constants.turret_position = position;
+
+    }
+
+    public void moveTurret(double ammount)
+    {
+        double newpos = Constants.turret_position + ammount;
+        turret.setPosition(newpos);
+        Constants.turret_position = newpos;
     }
 }
 
