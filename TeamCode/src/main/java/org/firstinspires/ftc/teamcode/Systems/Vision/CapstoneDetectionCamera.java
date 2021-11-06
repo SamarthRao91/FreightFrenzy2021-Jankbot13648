4dfcf8b6c7391e51dff6fa2a28220f2456355f79
Sidd Rao <siddrao.926@gmail.com>
2021-11-06 03:25:26 -0700

    Added Camera Pipeline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Vision/CapstoneDetectionCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Vision/CapstoneDetectionCamera.java
@@ -3,10 +3,13 @@ package org.firstinspires.ftc.teamcode.Systems.Vision;
 import static org.firstinspires.ftc.teamcode.Constants.Vision.CAPSTONE_DETECTION_CAMERA_NAME_LEFT;
 import static org.firstinspires.ftc.teamcode.Constants.Vision.CAPSTONE_DETECTION_CAMERA_NAME_RIGHT;
 
+import com.acmerobotics.dashboard.FtcDashboard;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.teamcode.Systems.Vision.Pipelines.CapstonePipeline;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
@@ -17,6 +20,7 @@ public class CapstoneDetectionCamera {
     WebcamName webcam1;
     WebcamName webcam2;
     OpenCvSwitchableWebcam switchableWebcam;
+    CapstonePipeline pipeline;
     Telemetry telemetry;
 
     public CapstoneDetectionCamera(HardwareMap hardwareMap) {
@@ -27,11 +31,16 @@ public class CapstoneDetectionCamera {
 
         switchableWebcam = OpenCvCameraFactory.getInstance().createSwitchableWebcam(cameraMonitorViewId, webcam1, webcam2);
 
+        pipeline = new CapstonePipeline();
+        switchableWebcam.setPipeline(pipeline);
+
+
         switchableWebcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
                                                    @Override
                                                    public void onOpened() {
                                                        //switchableWebcam.setPipeline();
-                                                       switchableWebcam.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+                                                       switchableWebcam.startStreaming(Constants.Vision.CAMERA_RESOLUTION_WIDTH, Constants.Vision.CAMERA_RESOLUTION_HEIGHT, OpenCvCameraRotation.UPRIGHT);
+                                                       FtcDashboard.getInstance().startCameraStream(switchableWebcam, 0);
                                                    }
 
                                                    @Override
