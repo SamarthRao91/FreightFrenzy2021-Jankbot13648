16d962562abf519086feff3e0ce67c17a6072eb8
Sidd Rao <siddrao.926@gmail.com>
2021-11-06 02:07:52 -0700

    Redid SafeTo Functions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Manipulator.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Manipulator.java
@@ -23,21 +23,21 @@ public class Manipulator {
         elevatorInstance = elevator[0];
     }
 
+    // TODO: TEST
     public boolean safeToTurret(double newPos)
     {
         //safe turret position is min safe
-       return elevatorInstance.getPosition() > Constants.Elevator.SAFE_TURRET_POSITION && (turret.getPosition() != Constants.Manipulator.Turret.ZERO_POSITION) || //blocks turret from extending in intake position
-            (extender.getPosition() < 0 || elevatorInstance.getPosition() > Constants.Elevator.SAFE_TURRET_POSITION) && elevatorInstance.getPosition() > Constants.Elevator.SAFE_EXTENDER_POSITION &&
-                    ((newPos < Constants.Manipulator.Turret.RIGHT_MAXIMUM_POSITION && turret.getPosition() > Constants.Manipulator.Turret.RIGHT_MAXIMUM_POSITION) || (newPos > Constants.Manipulator.Turret.LEFT_MAXIMUM_POSITION && turret.getPosition() < Constants.Manipulator.Turret.RIGHT_MAXIMUM_POSITION));
+       return (elevatorInstance.getPosition() > Constants.Elevator.SAFE_TURRET_POSITION && (getExtenderPosition() != Constants.Manipulator.Extender.MIN_POS && (newPos > 0.68 || newPos < 0.517)))
+               || elevatorInstance.getPosition() > Constants.Elevator.SAFE_EXTENDER_POSITION;
     }
 
     public boolean safeToExtender(double turretPos)
     {
-        return elevatorInstance.getPosition() > Constants.Elevator.SAFE_EXTENDER_POSITION ||
-                (turretPos > 0.68 || turretPos < 0.517);
+        return elevatorInstance.getPosition() > Constants.Elevator.SAFE_EXTENDER_POSITION
+                || (turretPos > 0.68 || turretPos < 0.517);
     }
 
-    public void setSuperStructure(int elevatorPos, int turretPos, int extenderPos, boolean isOpModeActive[])
+    public void setSuperStructure(int elevatorPos, int turretPos, int extenderPos, boolean[] isOpModeActive)
     {
         elevatorInstance.getElevatorMotor()[0].setTargetPosition(elevatorPos);
         elevatorInstance.getElevatorMotor()[0].setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -72,10 +72,15 @@ public class Manipulator {
 
     public void setTurretPosition(double newPos)
     {
-        turret.setPosition(newPos);
+        if(newPos > Constants.Manipulator.Turret.LEFT_MAXIMUM_POSITION)
+        {
+            turret.setPosition(Constants.Manipulator.Turret.LEFT_MAXIMUM_POSITION);
+        }
+
+        else
+            turret.setPosition(Math.max(newPos, Constants.Manipulator.Turret.RIGHT_MAXIMUM_POSITION));
     }
 
-    // TODO: Test With Hardware
     public void moveTurretPosition(double amount)
     {
         if(amount != 0)
@@ -92,7 +97,12 @@ public class Manipulator {
 
     public void setExtenderPosition(double newPos)
     {
-        extender.setPosition(newPos);
+        if(newPos < Constants.Manipulator.Extender.MIN_POS)
+        {
+            extender.setPosition(Constants.Manipulator.Extender.MIN_POS);
+        }
+        else
+            extender.setPosition(Math.min(newPos, Constants.Manipulator.Extender.MAX_POS));
     }
 
     public void moveExtenderPosition(double amount)
