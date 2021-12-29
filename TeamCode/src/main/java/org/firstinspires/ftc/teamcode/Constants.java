271322e1733474105c1df91924fcfaf42dc03040
Alex Y <yima@rsd407.org>
2021-12-28 17:31:59 -0800

    constant change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -75,7 +75,7 @@ public class Constants {
             public static final String DISTANCE_SENSOR_NAME = "sdc";
 
             public static double OPEN_POSITION = 0.50;
-            public static double CLOSE_POSITION = 0.32;
+            public static double CLOSE_POSITION = 0.1;
             public static final double CAPSTONE_OPEN_FULLY = 0.7;
         }
     }
