8ba4edf5c7e3bbb43401624438dcf84083795d60
Sidd Rao <siddrao.926@gmail.com>
2021-12-28 17:10:30 -0800

    Converted Manual Pickup Into Sequential Command Group

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ManipulatorCommands/ManualPickup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ManipulatorCommands/ManualPickup.java
@@ -1,27 +1,21 @@
 package org.firstinspires.ftc.teamcode.Commands.ManipulatorCommands;
 
-import com.arcrobotics.ftclib.command.CommandBase;
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
 
 import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.Systems.Manipulator;
 
-public class ManualPickup extends CommandBase {
-    private final Manipulator manipulator;
+public class ManualPickup extends SequentialCommandGroup {
 
     public ManualPickup(Manipulator manipulator) {
-        this.manipulator = manipulator;
+        addCommands(
+                new InstantCommand(manipulator::closeClaw),
+                new WaitCommand(100),
+                new InstantCommand(() -> manipulator.setArm(Constants.Manipulator.Arm.ARM1_LOWER_BOUND - 0.2))
+        );
 
         addRequirements(manipulator);
-    }
-
-    @Override
-    public void initialize() {
-        manipulator.closeClaw();
-        manipulator.setArm(Constants.Manipulator.Arm.ARM1_LOWER_BOUND - 0.2);
-    }
-
-    @Override
-    public boolean isFinished() {
-        return true;
     }
 }
