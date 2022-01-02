3492488eeb68d576ce4b24fb5b915b28f7a30f62
Alex Y <yima@rsd407.org>
2022-01-02 04:12:25 -0800

    Intake tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/AutoCommands/PushAndResetAndIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/AutoCommands/PushAndResetAndIntake.java
@@ -18,11 +18,12 @@ public class PushAndResetAndIntake extends SequentialCommandGroup {
                 new PushGamepiece(manipulator).alongWith(new InstantCommand(() -> intake.setIntake(.2))),
                 new ElevatorToPosition(elevator, 200, 1).alongWith(new InstantCommand(() -> manipulator.setArm(.6))),//slows turret if slides are far away
                 new TurretToPosition(manipulator, 0, 1).alongWith(new InstantCommand(() -> manipulator.setArm(Constants.Manipulator.Arm.ARM1_LOWER_BOUND - .2))).alongWith(new InstantCommand(() -> manipulator.setPusher(Constants.Manipulator.Pusher.PUSHER_UP_POS))).alongWith(new InstantCommand(() -> manipulator.closeClaw())).alongWith(new ElevatorToPosition(elevator, 0, 1)).alongWith(new InstantCommand(() -> intake.setIntake(-.2))),
+                new WaitCommand(250),
                 new InstantCommand(() -> manipulator.setArm(Constants.Manipulator.Arm.ARM1_LOWER_BOUND)),
-                new WaitCommand(375),
+                new WaitCommand(250),
                 new InstantCommand(() -> manipulator.openClaw()),
                 new InstantCommand(() -> intake.setIntake(1)),
-                new WaitCommand(250)
+                new WaitCommand(500)
 
 
         );
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/AutoCommands/Red/PickupAndHighPresetRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/AutoCommands/Red/PickupAndHighPresetRed.java
@@ -17,7 +17,8 @@ public class PickupAndHighPresetRed extends SequentialCommandGroup {
     public PickupAndHighPresetRed(Elevator elevator, Manipulator manipulator, Intake intake) {
         addCommands(
                 new ManualPickup(manipulator),
-                new WaitCommand(375),
+                new InstantCommand(() -> intake.setIntake(0)),
+                new WaitCommand(250),
                 new InstantCommand(() -> intake.setIntake(-.75)),
                 new TurretToPosition(manipulator, Constants.Manipulator.Turret.RIGHT_MAXIMUM_POSITION - 250, 1).alongWith(new InstantCommand(() -> manipulator.setArm(0.48))).alongWith(new InstantCommand(() -> manipulator.setPusher(Constants.Manipulator.Pusher.PUSHER_PREP_POS))).alongWith(new ElevatorToPosition(elevator, 150, 1)),
                 new ElevatorToPosition(elevator, 1000, 1),
