db22e8ff2ca881c2c440c0a5af487ed0fca213c9
Alex Y <yima@rsd407.org>
2022-01-04 02:15:57 -0800

    Red cycle auto tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/AutoCommands/PushAndResetAndIntakeRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/AutoCommands/PushAndResetAndIntakeRed.java
@@ -16,9 +16,10 @@ public class PushAndResetAndIntake extends SequentialCommandGroup {
     public PushAndResetAndIntake(Elevator elevator, Manipulator manipulator, Intake intake) {
         addCommands(
                 new PushGamepiece(manipulator).alongWith(new InstantCommand(() -> intake.setIntake(.2))),
-                new ElevatorToPosition(elevator, 200, 1).alongWith(new InstantCommand(() -> manipulator.setArm(.6))),//slows turret if slides are far away
-                new TurretToPosition(manipulator, 0, 1).alongWith(new InstantCommand(() -> manipulator.setArm(Constants.Manipulator.Arm.ARM1_LOWER_BOUND - .2))).alongWith(new InstantCommand(() -> manipulator.setPusher(Constants.Manipulator.Pusher.PUSHER_UP_POS))).alongWith(new InstantCommand(() -> manipulator.closeClaw())).alongWith(new ElevatorToPosition(elevator, 0, 1)).alongWith(new InstantCommand(() -> intake.setIntake(-.2))),
-                new WaitCommand(250),
+                new WaitCommand(10),
+                new ElevatorToPosition(elevator, 200, 1).alongWith(new InstantCommand(() -> manipulator.setArm(.65))),//slows turret if slides are far away
+                new TurretToPosition(manipulator, 0, 1).alongWith(new InstantCommand(() -> manipulator.setPusher(Constants.Manipulator.Pusher.PUSHER_UP_POS))).alongWith(new InstantCommand(() -> manipulator.closeClaw())).alongWith(new ElevatorToPosition(elevator, 0, 1)).alongWith(new InstantCommand(() -> intake.setIntake(-.2))),
+                new WaitCommand(1),
                 new InstantCommand(() -> manipulator.setArm(Constants.Manipulator.Arm.ARM1_LOWER_BOUND)),
                 new WaitCommand(250),
                 new InstantCommand(() -> manipulator.openClaw()),
