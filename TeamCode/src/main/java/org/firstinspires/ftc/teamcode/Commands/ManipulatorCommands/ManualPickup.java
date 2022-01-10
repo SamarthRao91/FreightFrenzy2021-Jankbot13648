9b46eff3ee05bfd29863edea428086d7d9ea2c19
Samarth Rao <raosamarth91@gmail.com>
2022-01-10 09:36:01 -0800

    Added Reverse Intake to Manual Pickup

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ManipulatorCommands/ManualPickup.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ManipulatorCommands/ManualPickup.java
@@ -6,14 +6,19 @@ import com.arcrobotics.ftclib.command.WaitCommand;
 
 import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.Systems.Manipulator;
+import org.firstinspires.ftc.teamcode.Systems.Intake;
 
 public class ManualPickup extends SequentialCommandGroup {
 
-    public ManualPickup(Manipulator manipulator) {
+    public ManualPickup(Manipulator manipulator, Intake intake) {
         addCommands(
                 new InstantCommand(manipulator::closeClaw),
                 new WaitCommand(100),
-                new InstantCommand(() -> manipulator.setArm(Constants.Manipulator.Arm.ARM1_LOWER_BOUND - 0.4))
+                new InstantCommand(() -> manipulator.setArm(Constants.Manipulator.Arm.ARM1_LOWER_BOUND - 0.4)),
+                new InstantCommand(() -> intake.setIntake(-0.8)),
+                new WaitCommand(100),
+                new InstantCommand(() -> intake.setIntake(0))
+
         );
 
         manipulator.manualPickUp = true;
