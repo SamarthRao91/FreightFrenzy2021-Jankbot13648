3d900587e2f0d008bf280b5886563135e32ec9de
Samarth Rao <raosamarth91@gmail.com>
2022-01-10 16:12:26 -0800

    Revert "Reverse Intake After Auto Pickup"
    This reverts commit 37726a03f37403b7fc2613cdeac7ef6e30faf746.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ManipulatorCommands/ManipulatorDefault.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Commands/ManipulatorCommands/ManipulatorDefault.java
@@ -7,8 +7,6 @@ import com.arcrobotics.ftclib.command.WaitCommand;
 import org.firstinspires.ftc.teamcode.Constants;
 import org.firstinspires.ftc.teamcode.Systems.Elevator;
 import org.firstinspires.ftc.teamcode.Systems.Manipulator;
-import org.firstinspires.ftc.teamcode.Systems.Intake;
-
 
 import java.util.function.BooleanSupplier;
 import java.util.function.DoubleSupplier;
@@ -21,13 +19,11 @@ public class ManipulatorDefault extends CommandBase {
 
     private final DoubleSupplier speed;
 
-    private final Intake intake;
-
-
-    public ManipulatorDefault(Manipulator manipulator, DoubleSupplier leftStickY, DoubleSupplier speed, Intake intake) {
+    public ManipulatorDefault(Manipulator manipulator, DoubleSupplier leftStickY, DoubleSupplier speed)
+    {
         this.manipulator = manipulator;
         this.speed = speed;
-        this.intake = intake;
+
         this.leftStickY = leftStickY;
 
         addRequirements(manipulator);
@@ -36,18 +32,14 @@ public class ManipulatorDefault extends CommandBase {
 
     @Override
     public void execute() {
-        manipulator.moveArm(-leftStickY.getAsDouble() / 100);
+        manipulator.moveArm(-leftStickY.getAsDouble()/100);
         manipulator.setSpeed(speed.getAsDouble());
-        if (manipulator.readColorSensor() > 600) {
+        if (manipulator.readColorSensor() > 600)
+        {
             manipulator.closeClaw();
             new WaitCommand(250);
             manipulator.setArm(Constants.Manipulator.Arm.ARM1_LOWER_BOUND - 0.4);
-            intake.setIntake(-0.8);
-            new WaitCommand(100);
-            intake.setIntake(0);
         }
-
-
     }
 
 
